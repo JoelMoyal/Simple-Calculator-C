a66673dccd4e1dd85c96d2e10685e8bb61ccdb15
Joel Moyal <j.moyal@student.fontys.nl>
2024-04-15 15:31:06 -0600

    adding main

---
+++ main.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+printf (" this man was having the name john \n") ;
+printf ("   lalalala|\n") ;
+printf ("   answer is kakak|\n") ;
+printf ("   no sshi aisja  |\n") ;
+printf (" eteuszgsuzabx|\n") ;
+
+ return 0 ;
+
+}
