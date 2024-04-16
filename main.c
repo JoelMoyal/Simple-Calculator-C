2b61da33e6f82bbad7ebf9680143002ec44b1481
Joel Moyal <j.moyal@student.fontys.nl>
2024-04-16 15:22:44 -0600

    adding easy calculator

--- main.c
+++ main.c
@@ -5,8 +5,13 @@ int main() {
     
     int num1;
     int num2;
-    printf ("Enter first number: ")
-    
+    printf ("Enter first number: ");
+    scanf ("%d", &num1) ;
+
+    printf ("Enter first number: ");
+    scanf ("%d", &num2);
+
+    printf ("Answer: %d", num1 + num2 );
 
     return 0;
 }
